21894b634e298f1397bcbf8b2f3e26c63a588d87
VAzenx <wutthipong.2006x@gmail.com>
2025-06-24 16:52:04 +0700

    Hello in new dev

---
+++ Lacture-01/print-test.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+print("This is my first Python program.")
+print("I am learning Python \"programming\".")
+print('/\\')
+print('\ta\na\ta\ta\t\n\ta')
--- Lacture-01/test-01.py
+++ Lacture-01/test-01.py
@@ -1,7 +1,16 @@
-print("Hello, World!")
-print("This is my first Python program.")
-print("I am learning Python \"programming\".")
-print('/\\')
+from math import pi
 
-print('\ta\na\ta\ta\t\n\ta')
+print("%d" %100) #decimal
+print("%d" %100.11) #decimal with float
+print("%f" %-100.111) #decimal with float
+print("%.2f" %100.111) #float with 2 decimal places
+print("%.4f" %pi) #string with decimal
+print("5+4" ,"= %.2f" %(5+4)) #string with calculation
 
+
+# Using format specifiers to print variables
+age = 25
+price = 3500
+id = 1 
+
+print("My age is %d years and the price is %d bath." %(age, price) + " ID: %d" % id)
