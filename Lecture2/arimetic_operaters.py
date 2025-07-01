11ebf05e58238d399e73b2a7f7731bbf8eb71e01
VAzenx <wutthipong.2006x@gmail.com>
2025-07-01 15:02:34 +0700

    add commit

---
+++ Lecture2/arimetic_operaters.py
@@ -0,0 +1,17 @@
+x = 15
+y = 4
+
+# Arithmetic Operators
+print("x + y =", x + y)  # Addition
+
+print("x - y =", x - y)  # Subtraction'
+
+print("x * y =", x * y)  # Multiplication
+
+print("x / y =", x / y)  # Division
+
+print("x % y =", x % y)  # Modulus (Remainder)
+
+print("x // y =", x // y)  # Floor Division
+
+print('x ** y =', x ** y)  # Exponentiation
---
+++ Lecture2/type_convertsion.py
@@ -0,0 +1,12 @@
+# Type Conversion in Python
+age = 25 
+height = float(age)
+print("Height:", height)
+
+height = 5.6
+age = int(height)
+print("Age:", age)
+
+num_str = "123"
+num_int = int(num_str)
+
